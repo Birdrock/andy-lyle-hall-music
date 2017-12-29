c356cc294412c7b64afb17bc3ac30db9a4644807
Andrew Wittrock <awittrock@gmail.com>
2017-12-28 21:43:05 -0800

    Basic scaffolding with some ipsum text to build out elements.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,101 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Link, Route, Redirect} from 'react-router-dom';
+import Switch from 'react-router-dom/Switch';
+
+const HeaderLink = ( {text, route} ) => {
+  return (
+    <div className="header-link">
+      <Link to={route}><span>{text}</span></Link>
+    </div>
+  )
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <p>
+          Yar Pirate Ipsum
+        </p>
+        <p>
+          Jack grog blossom heave to American Main Yellow Jack topsail spike overhaul chase. Broadside galleon hardtack pirate hands pink brig pressgang rutters. Coxswain hardtack Sea Legs come about warp topgallant Davy Jones' Locker pinnace marooned.
+        </p>
+        <p>
+          Stern Cat o'nine tails landlubber or just lubber quarter belaying pin hail-shot grog blossom yo-ho-ho trysail. Dance the hempen jig fire in the hole matey fore barque deadlights schooner no prey, no pay loot. Chase draft gun pillage walk the plank hail-shot boom mizzenmast lass.
+        </p>
+      </div>
+    )
+  }
+}
+
+class Music extends Component {
+  render() {
+    return (
+      <div>Music</div>
+    )
+  }
+}
+class Video extends Component {
+  render() {
+    return (
+      <div>Video</div>
+    )
+  }
+}
+class Shows extends Component {
+  render() {
+    return (
+      <div>Shows</div>
+    )
+  }
+}
+class Album extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Mighty Tree</h2>
+        <p>
+          Let us wax poetic about the beauty of the cheeseburger. The flavour gracefully dances across your palate like a majestic figure skater on ice.
+        </p>
+        <p>
+          The cheese melts on the burger and in your mouth, perfectly complementing the medium-rare beef. Any burger lover worth their salt knows the best patty is comprised of ground chuck and brisket.
+        </p>
+        <p>
+          Whether you’re a ketchup and mustard loyalist or you prefer house sauce to no sauce, you can agree that every cheeseburger has it’s own special flair. Top tip: house sauce begs to be ordered on the side for fry-dipping purposes.
+        </p>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <div className="andy-lyle-hall"><span>Andy Lyle Hall</span></div>
+        <div className="header-link-container">
+          <HeaderLink text={"Home"} route={"/"} />
+          <HeaderLink text={"Album"} route={"/album"} />
+          <HeaderLink text={"Music"} route={"/music"} />
+          <HeaderLink text={"Video"} route={"/video"} />
+          <HeaderLink text={"Shows"} route={"/shows"} />
+        </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="body-container">
+          <Switch>
+            <Route path="/music" component={Music} />
+            <Route path="/album" component={Album} />
+            <Route path="/video" component={Video} />
+            <Route path="/shows" component={Shows} />
+            <Route path="/" component={Home} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter,
+         Link,
+         Route
+       } from 'react-router-dom'
+
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
